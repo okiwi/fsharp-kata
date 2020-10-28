ae87beb958303cea3242ac5020fcb124e99452bc
Jean-Baptiste Dusseaut <157962+BodySplash@users.noreply.github.com>
2020-10-28 16:43:32 +0100

    next

--- Lib.fs
+++ Lib.fs
@@ -2,5 +2,16 @@ module Lib
 
 open System
 
-let wordWrap (text:string) (width:int) = 
-    text.Replace(' ', '\n')
+let concat (width: int) (acc: string) (value: string) =
+    match acc with
+    | "" -> value
+    | _ -> if acc.Length + value.Length >= width then acc + "\n" + value else acc + " " + value
+
+
+let wordWrap (text: string) (width: int) =
+    match text with
+    | "" -> ""
+    | _ ->
+        text.Split " "
+        |> Seq.toList
+        |> Seq.fold (concat width) ""
--- Tests.fs
+++ Tests.fs
@@ -11,14 +11,18 @@ let tests =
       Expect.equal result "" "Failed";
     
     testCase "should not wrap text shorter than width" <| fun _ ->
-      let result = wordWrap "word" 6
-      Expect.equal result "word" "Failed";
+      let result = wordWrap "word word" 10
+      Expect.equal result "word word" "Failed";
 
     testCase "should wrap text longer than width" <| fun _ ->
       let result = wordWrap "word word" 6
-      Expect.equal result "word\nword" "Failed";
-
+      Expect.equal result "word\nword" "Failed"
+      
     testCase "should wrap text longer than width but not all words" <| fun _ ->
       let result = wordWrap "word word word" 10
-      Expect.equal result "word word\nword" "Failed";
+      Expect.equal result "word word\nword" "Failed"
+      
+    testCase "should deal with white spaces" <| fun _ ->
+      let result = wordWrap "word word word         " 10
+      Expect.equal result "word word\nword" "Failed"
   ]
